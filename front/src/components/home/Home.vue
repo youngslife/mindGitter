1a2008e60f59a8f0688780b68344aac5ec8dae9f
JaehyunKim94 <didwjd169@gmail.com>
2020-04-14 17:50:50 +0900

    S02P23A302-5 # install Vuetify & Home init

--- front/src/components/home/Home.vue
+++ front/src/components/home/Home.vue
@@ -1,11 +1,62 @@
 <template>
-  <div>
-    <h2>Home Page</h2>
-  </div>
+  <v-row>
+    <v-carousel :show-arrows="carOption">
+      <v-carousel-item v-for="(item, i) in diaryList" :key="i" :src="item.src">
+      </v-carousel-item>
+      <v-carousel-item :src="addImg">
+        <button @click="goCreate">Add Diary</button>
+      </v-carousel-item>
+    </v-carousel>
+  </v-row>
 </template>
 
 <script>
-export default {};
+import router from "@/router";
+// import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      carOption: false,
+      diaryList: [],
+      addImg: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+      isLoggedIn: true
+    };
+  },
+  methods: {
+    // 유저의 일기 목록 가져오기
+    getDiary() {
+      const gotList = [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg"
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg"
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg"
+        }
+      ];
+      this.diaryList = gotList;
+    },
+    goCreate() {
+      router.push("createDiary");
+    }
+  },
+  // // vuex Login 연동
+  // computed: {
+  //   ...mapGetters(["isLoggedIn"])
+  // },
+  created() {
+    if (this.isLoggedIn) {
+      this.getDiary();
+    } else {
+      router.push("/login");
+    }
+  }
+};
 </script>
 
 <style scoped src="./home.css"></style>
