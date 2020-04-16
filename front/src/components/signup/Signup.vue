fa187d2dd55c2d78ec2b5d7d8ed06640c0c8c160
jisunlee <jisunl0423@gmail.com>
2020-04-16 10:35:20 +0900

    S02P23A302-9 # Signup design

---
+++ front/src/components/signup/Signup.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Login">
+    <form>
+      <h1>Signup</h1>
+      <ul id="userId">
+        ID
+        <input type="text" id="userId" placeholder="아이디" />
+      </ul>
+      <ul id="userEmail">
+        e-mail
+        <input type="text" id="userEmail" placeholder="e-mail" />
+      </ul>
+      <ul id="userPassword">
+        Password
+        <input type="password" id="userPassword" placeholder="비밀번호" />
+      </ul>
+      <ul id="userPasswordConfirmation">
+        Password
+        <input
+          type="password"
+          id="userPasswordConfirmation"
+          placeholder="비밀번호확인"
+        />
+      </ul>
+      <a href="/login">Login</a>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style src="./Signup.css" scoped></style>
