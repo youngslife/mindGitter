19c254c3b197e8526561656722238e390e1e0e4c
jisunlee <jisunl0423@gmail.com>
2020-04-20 11:56:21 +0900

    S02P23A302-16 #layout of userDetail page except commit calendar
    Former-commit-id: a55aeef6e710b1a444009457d328ba0ddf6133bd

--- front/src/components/userDetail/UserDetail.vue
+++ front/src/components/userDetail/UserDetail.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="userDetail">
     <div class="userId">
-      <p>
+      <p v-if="isLoggedIn">
         {{ getUserName }}
       </p>
-      <button class="settings">
-        <v-icon>fas fa-cog</v-icon>
+      <button class="settings" @click.prevent="logout">
+        <v-icon>fas fa-sign-out-alt</v-icon>
       </button>
     </div>
     <img
@@ -13,13 +13,39 @@
       src="../../assets/userprofile.jpg"
       alt="userprofile"
     />
+    <div class="totalInfo">
+      <v-row>
+        <v-col>
+          First
+        </v-col>
+        <v-col>
+          Total
+        </v-col>
+        <v-col>
+          Channel
+        </v-col>
+      </v-row>
+      무슨 정보를 넣으면 좋겠다
+    </div>
+    <div class="emotion">
+      <h3>Emotion</h3>
+      <div class="emotionDetail">
+        감정 분석 결과
+      </div>
+    </div>
+    <div class="tag">
+      <h3>Tag</h3>
+      <div class="tagDetail">
+        tag 분석 결과
+      </div>
+    </div>
     <Nav />
   </div>
 </template>
 
 <script>
 import Nav from "../nav/Nav.vue";
-import { mapGetters } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "UserDetail",
@@ -27,7 +53,10 @@ export default {
     Nav
   },
   computed: {
-    ...mapGetters(["getUserName"])
+    ...mapGetters(["getUserName", "isLoggedIn"])
+  },
+  methods: {
+    ...mapActions(["logout"])
   }
 };
 </script>
