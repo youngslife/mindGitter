db3b501d045f5851e419b9a3b9198e04f3b670e4
JaehyunKim94 <didwjd169@gmail.com>
2020-04-21 16:11:21 +0900

    S02P23A302-32 # user detail page CSS - responsive init
    Former-commit-id: cf8053d28cd33aaed283dd1fe1382d4a459c3b34

--- front/src/components/userDetail/UserDetail.vue
+++ front/src/components/userDetail/UserDetail.vue
@@ -1,79 +1,47 @@
 <template>
   <div class="userDetail">
-    <div class="userId">
-      <p v-if="isLoggedIn">
-        {{ getUserName }}
-      </p>
-      <button class="settings" @click.prevent="logout">
-        <v-icon>fas fa-sign-out-alt</v-icon>
-      </button>
-    </div>
-    <img
-      class="userprofile"
-      src="../../assets/userprofile.jpg"
-      alt="userprofile"
-    />
-    <div class="totalInfo">
-      <v-row>
-        <v-col>
-          First
-        </v-col>
-        <v-col>
-          Total
-        </v-col>
-        <v-col>
-          Channel
-        </v-col>
-      </v-row>
-      무슨 정보를 넣으면 좋겠다
-    </div>
-    <div class="commitCalendar">
-      <CalendarHeatmap
-        endDate="2020-12-31"
-        :values="[
-          { date: '2020-04-20', count: 5 },
-          { date: '2020-04-19', count: 3 },
-          { date: '2020-04-17', count: 4 },
-          { date: '2020-04-15', count: 2 },
-          { date: '2020-04-12', count: 1 },
-          { date: '2020-04-10', count: 4 },
-          { date: '2020-04-6', count: 6 }
-        ]"
-      />
-    </div>
-    <div class="emotion">
-      <h3>Emotion</h3>
-      <div class="emotionDetail">
-        감정 분석 결과
+    <v-container class="userCon">
+      <UserHead />
+      <UserInfo />
+      <!-- <CommitCalendar /> -->
+      <div class="emotion">
+        <h3>Emotion</h3>
+        <div class="emotionDetail">
+          감정 분석 결과
+        </div>
       </div>
-    </div>
-    <div class="tag">
-      <h3>Tag</h3>
-      <div class="tagDetail">
-        tag 분석 결과
+      <div class="tag">
+        <h3>Tag</h3>
+        <div class="tagDetail">
+          tag 분석 결과
+        </div>
       </div>
-    </div>
+    </v-container>
     <Nav />
   </div>
 </template>
 
 <script>
 import Nav from "../nav/Nav.vue";
-import { CalendarHeatmap } from "vue-calendar-heatmap";
+import UserHead from "./userInfo/UserHead.vue";
+import UserInfo from "./userInfo/UserInfo.vue";
+// import CommitCalendar from "./userInfo/CommitCalendar";
 import { mapGetters, mapActions } from "vuex";
 
 export default {
   name: "UserDetail",
   components: {
     Nav,
-    CalendarHeatmap
+    UserHead,
+    UserInfo,
+    // CommitCalendar,
   },
   computed: {
-    ...mapGetters(["getUserName", "isLoggedIn"])
+    ...mapGetters(["getUserName", "isLoggedIn"]),
   },
   methods: {
-    ...mapActions(["logout"])
-  }
+    ...mapActions(["logout"]),
+  },
 };
 </script>
 
---
+++ front/src/components/userDetail/userInfo/UserInfo.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col cols="3">
+      <img
+        class="userProfile"
+        src="../../../assets/userprofile.jpg"
+        alt="userprofile"
+      />
+    </v-col>
+    <v-row class="userSummary" cols="9">
+      <v-col cols="4">Total</v-col>
+      <v-col cols="4">Create</v-col>
+      <v-col cols="4">Diary</v-col>
+    </v-row>
+  </v-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style src="./UserInfo.css" scoped></style>
