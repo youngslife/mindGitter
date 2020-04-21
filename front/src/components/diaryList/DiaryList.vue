0c01bf2cb1faedd0d522b217ad82eb9cc8cb4d1c
jisunlee <jisunl0423@gmail.com>
2020-04-21 15:56:01 +0900

    S02P23A302-33 #diaryList page layout
    Former-commit-id: 0b236f6bc99b202fe1ea5ba0c51c79640a76a937

--- front/src/components/diaryList/DiaryList.vue
+++ front/src/components/diaryList/DiaryList.vue
@@ -2,10 +2,23 @@
   <div class="diaryList">
     <div class="infoAndSetting">
       <h1>{{ getSelectedChan }}</h1>
-      <v-icon>fas fa-user-plus</v-icon>
+      <v-icon class="plus" @click="changeShowAddModal">fas fa-user-plus</v-icon>
+      <v-card v-if="showAddModal" @close="showAddModal = false">
+        <v-card-title>Share Diary</v-card-title>
+        <v-card-text>
+          친구 아이디
+        </v-card-text>
+        <input />
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn class="search" @click="changeShowAddModal">공유</v-btn>
+          <v-btn class="close" @click="changeShowAddModal">닫기</v-btn>
+        </v-card-actions>
+      </v-card>
+      <v-icon class="delete" @click="deleteChannel">fas fa-trash-alt</v-icon>
     </div>
     <div calss="search">
-      <v-icon>fas fa-search</v-icon>
+      <v-icon class="search" v-if="!showAddModal">fas fa-search</v-icon>
       <input type="text" v-model="searchTag" />
       <div class="sharedImage">
         <img
@@ -13,6 +26,55 @@
           src="../../assets/shareduserprofile.jpg"
           alt="sharedUserProfile"
         />
+      </div>
+    </div>
+    <div class="calendar">
+      달력....
+    </div>
+    <div class="diaries">
+      <div class="diaryInfo">
+        <img
+          src="../../assets/shareduserprofile.jpg"
+          alt="userProfile"
+          class="uImage"
+        />
+        <div class="content">
+          <p class="title">첫 번째</p>
+          <span class="tag">#첫 일기 #행복함 #재밌음</span>
+        </div>
+      </div>
+      <div class="diaryInfo">
+        <img
+          src="../../assets/userprofile.jpg"
+          alt="userProfile"
+          class="uImage"
+        />
+        <div class="content">
+          <p class="title">세 번째</p>
+          <span class="tag">#공유신청 #기대됨 #얼른</span>
+        </div>
+      </div>
+      <div class="diaryInfo">
+        <img
+          src="../../assets/userprofile.jpg"
+          alt="userProfile"
+          class="uImage"
+        />
+        <div class="content">
+          <p class="title">두 번째</p>
+          <span class="tag">#혼자심심 #공유할까? #지루함</span>
+        </div>
+      </div>
+      <div class="diaryInfo">
+        <img
+          src="../../assets/userprofile.jpg"
+          alt="userProfile"
+          class="uImage"
+        />
+        <div class="content">
+          <p class="title">일기장 만들었다</p>
+          <span class="tag">#내가주인 #놀라움 #공유</span>
+        </div>
       </div>
     </div>
     <Nav />
@@ -28,7 +90,8 @@ export default {
   data() {
     return {
       searchTag: null,
-      sharedUser: "멋찌"
+      date: new Date(),
+      showAddModal: false
     };
   },
   components: {
@@ -36,6 +99,22 @@ export default {
   },
   computed: {
     ...mapGetters(["getSelectedChan"])
+  },
+  methods: {
+    changeShowAddModal() {
+      this.showAddModal = !this.showAddModal;
+    },
+    deleteChannel() {
+      if (
+        confirm(
+          "일기장이 삭제되면 지금까지 작성하신 일기가 모두 삭제됩니다.\n삭제하시겠습니까?"
+        )
+      ) {
+        console.log("삭제");
+      } else {
+        console.log("취소");
+      }
+    }
   }
 };
 </script>
