5cb2feed7e303bbb925109f619150a9730d01f81
JaehyunKim94 <didwjd169@gmail.com>
2020-04-23 15:12:48 +0900

    S02P23A302-46 # Create post page init
    Former-commit-id: 7384fc4a7d8b888ff0e0ab93b992e07420f5541c

--- front/src/store/modules/diary.js
+++ front/src/store/modules/diary.js
@@ -5,17 +5,17 @@ import router from "../../router";
 
 const state = {
   selectedChan: null,
-  selectedDiary: null
+  selectedDiary: null,
 };
 
 const getters = {
-  getSelectedChan: state => state.selectedChan,
-  getSelectedDiary: state => state.selectedDiary
+  getSelectedChan: (state) => state.selectedChan,
+  getSelectedDiary: (state) => state.selectedDiary,
 };
 
 const mutations = {
   setSelectedChan: (state, channel) => (state.selectedChan = channel),
-  setSelectedDiary: (state, diary) => (state.selectedDiary = diary)
+  setSelectedDiary: (state, diary) => (state.selectedDiary = diary),
 };
 
 const actions = {
@@ -24,26 +24,30 @@ const actions = {
       .post(HOST + "?", DiaryInfo, {
         headers: {
           "Content-Type": "application/json",
-          Accept: "application/json"
-        }
+          Accept: "application/json",
+        },
       })
-      .then(message => {
+      .then((message) => {
         message;
         router.push("/postList");
       })
-      .catch(err => {
+      .catch((err) => {
         if (!err.response) {
           commit("pushError", "Network Error..");
         } else {
           commit("pushError", "Some error occured");
         }
       });
-  }
+  },
+  addPost: ({ commit }, PostInfo) => {
+    console.log(PostInfo)
+    commit("pushError", "Success")
+  },
 };
 
 export default {
   state,
   getters,
   mutations,
-  actions
+  actions,
 };
