c3af40c578c51891c6b92983cef3ee76a129e91d
Yunyoung Chung <double.y.0525@gmail.com>
2020-04-25 02:53:31 +0900

    S02P23A302-50 # filtering posts as date
    Former-commit-id: 2695eb75c5f7acb9570ec653da4832090ba6b84a

--- BACK/posts/serializers.py
+++ BACK/posts/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from .models import Post, Emotion, Tag, Comment
 from accounts.models import User, UserEmotion
 from taggit_serializer.serializers import (TaggitSerializer, TagListSerializerField)
-from accounts.serializers import UserTagSerializer, UserDisplaySerializer
+# from accounts.serializers import UserTagSerializer, UserDisplaySerializer
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,11 +25,20 @@ class EmotionSerializer(serializers.ModelSerializer):
         model = Emotion
         fields = '__all__'
 
+# for calendar filtering
+class PostDateFilterSerializer(serializers.ListSerializer):
+    def to_representation(self, data):
+        today = datetime.now()
+        three_month_ago = today + relativedelta(months=-6)
+        data = data.filter(created_at__lte=today).filter(created_at__gte=three_month_ago)
+        return super(PostDateFilterSerializer, self).to_representation(data)
+
 
 class PostSerializer(TaggitSerializer, serializers.ModelSerializer):
     comment_set = CommentSerializer(read_only=True, many=True)
     tags = TagListSerializerField()
     class Meta:
+        list_serializer_class = PostDateFilterSerializer
         model = Post
         fields = ('pk', 'title', 'cover_image', 'user_id',
                     'context', 'created_at', 'updated_at', 
