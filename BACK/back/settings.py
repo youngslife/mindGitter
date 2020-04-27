be2631f0feed5b576ef2262d1aaff10fb9b65f17
Leo Kim <gh.leokim@gmail.com>
2020-04-27 10:56:43 +0900

    S02P23A302-47 # update static and media url settings
    Former-commit-id: f8bd4e5a5b25c855357f0508dcb561c9d67bd6e9

--- BACK/back/settings.py
+++ BACK/back/settings.py
@@ -201,7 +201,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/api/static/'
 
 AUTH_USER_MODEL = 'accounts.User'
 
@@ -217,7 +217,7 @@ OLD_PASSWORD_FIELD_ENABLED = True
 LOGOUT_ON_PASSWORD_CHANGE = False
 
 # media
-MEDIA_URL = '/media/'
+MEDIA_URL = '/api/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Tag
