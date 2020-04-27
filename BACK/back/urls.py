d794e7afa8baad601a71fb69a08e217573bf59f3
Yunyoung Chung <double.y.0525@gmail.com>
2020-04-28 00:07:47 +0900

    S02P23A302-50 # revise some errors
    Former-commit-id: d5a1a377cb87115bfaad8bc77bcd071d7c58597e

--- BACK/back/urls.py
+++ BACK/back/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('accounts.urls')),
+    path('', include('accounts.urls')),
     path("api-auth/", include("rest_framework.urls")),
     # login, registration등 path 설정
     path("rest-auth/", include("rest_auth.urls")),
--- BACK/posts/serializers.py
+++ BACK/posts/serializers.py
@@ -31,19 +31,21 @@ class PostDateFilterSerializer(serializers.ListSerializer):
         today = datetime.now()
         six_months_ago = today + relativedelta(months=-6)
         data = data.filter(created_at__lte=today).filter(created_at__gte=six_months_ago)
-        first_p = data[0]
-        list_post_created = [{ first_p.created_at.strftime('%Y-%m-%d'): [first_p.id] }]
+        if data:
+            first_p = data[0]
+            list_post_created = [{ first_p.created_at.strftime('%Y-%m-%d'): [first_p.id] }]
 
-        for posting in data[1:]: # 각 포스트 데이터
-            ddate = posting.created_at.strftime('%Y-%m-%d')
-            for post_created in list_post_created:
-                if ddate in post_created.keys():
-                    post_created[ddate].append(posting.id); break
-                else:
-                    list_post_created.append({ddate: [posting.id]}); break
+            for posting in data[1:]: # 각 포스트 데이터
+                ddate = posting.created_at.strftime('%Y-%m-%d')
+                for post_created in list_post_created:
+                    if ddate in post_created.keys():
+                        post_created[ddate].append(posting.id); break
+                    else:
+                        list_post_created.append({ddate: [posting.id]}); break
 
-        return list_post_created
-        return super(PostDateFilterSerializer, self).to_representation(data)
+            return list_post_created
+        else:
+            return super(PostDateFilterSerializer, self).to_representation(data)
 
 
 class PostSerializer(TaggitSerializer, serializers.ModelSerializer):
