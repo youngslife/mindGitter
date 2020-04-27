94f6a8a0cd742b1140770d1ed44f7b0dea38b2e8
Yunyoung Chung <double.y.0525@gmail.com>
2020-04-27 20:11:27 +0900

    S02P23A302-50 # serialize as date
    Former-commit-id: 677cb9863d763aff80cf6da27fbae47c5a0c777a

--- BACK/posts/serializers.py
+++ BACK/posts/serializers.py
@@ -29,8 +29,20 @@ class EmotionSerializer(serializers.ModelSerializer):
 class PostDateFilterSerializer(serializers.ListSerializer):
     def to_representation(self, data):
         today = datetime.now()
-        three_month_ago = today + relativedelta(months=-6)
-        data = data.filter(created_at__lte=today).filter(created_at__gte=three_month_ago)
+        six_months_ago = today + relativedelta(months=-6)
+        data = data.filter(created_at__lte=today).filter(created_at__gte=six_months_ago)
+        first_p = data[0]
+        list_post_created = [{ first_p.created_at.strftime('%Y-%m-%d'): [first_p.id] }]
+
+        for posting in data[1:]: # 각 포스트 데이터
+            ddate = posting.created_at.strftime('%Y-%m-%d')
+            for post_created in list_post_created:
+                if ddate in post_created.keys():
+                    post_created[ddate].append(posting.id); break
+                else:
+                    list_post_created.append({ddate: [posting.id]}); break
+
+        return list_post_created
         return super(PostDateFilterSerializer, self).to_representation(data)
 
 
