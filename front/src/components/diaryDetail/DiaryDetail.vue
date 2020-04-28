0478bff6355f25d372f256494efb818f8ac43542
jisunlee <jisunl0423@gmail.com>
2020-04-28 15:45:39 +0900

    show differently according to options
    Former-commit-id: 456f5242ece78da26767ac5fb601acfc46d28523

--- front/src/components/diaryDetail/DiaryDetail.vue
+++ front/src/components/diaryDetail/DiaryDetail.vue
@@ -11,10 +11,16 @@
       </div>
     </v-card>
     <h1>{{ getSelectedChan.title }}</h1>
-    <video width="100%" height="255px" controls class="dvideo">
+    <video
+      width="100%"
+      height="255px"
+      controls
+      class="dvideo"
+      v-if="selectedDiary.is_save_video"
+    >
       <source :src="videoAddr + selectedDiary.video_file" />
     </video>
-    <div class="temp"></div>
+    <div class="temp" v-if="selectedDiary.is_save_video"></div>
     <v-tabs fixed-tabs>
       <v-tab v-for="mode in modes" :key="mode" @click="changeMode(mode)">{{
         mode
@@ -22,7 +28,7 @@
     </v-tabs>
     <div class="text" v-if="selectedMode == 'Detail'">
       <h3>{{ selectedDiary.title }}</h3>
-      <div class="tags">
+      <div class="tags" v-if="selectedDiary.tags[0] != 'null'">
         <span v-for="(tag, idx) in selectedDiary.tags" :key="idx"
           >#{{ tag }}
         </span>
@@ -35,7 +41,12 @@
       <span>{{ selectedMode }}</span>
     </div>
     <div class="text" v-if="selectedMode == 'Comment'">
-      <span>{{ selectedMode }}</span>
+      <div v-if="selectedDiary.is_use_comment">
+        <span>{{ selectedMode }}</span>
+      </div>
+      <div v-else>
+        <span>{{ selectedMode }}를 허용하지 않습니다.</span>
+      </div>
     </div>
     <div class="additionalInfo">
       <span>작성자: {{ getWriterInfo.username }}</span>
