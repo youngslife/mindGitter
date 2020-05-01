ebc58acf722c2a0ef49aea5473d65b81de672ee8
jisunlee <jisunl0423@gmail.com>
2020-05-01 14:45:51 +0900

    merge
    Former-commit-id: d0822527cd4a03e4fde446b8d708c405eeb428df

--- front/src/components/editDiary/editDiary.vue
+++ front/src/components/editDiary/editDiary.vue
@@ -1,38 +1,73 @@
 <template>
-  <div class="createDiary">
-    <v-icon class="back" @click="goHome">fas fa-arrow-left</v-icon>
-    <form class="AddDiaryForm" @submit.prevent="editChannel(PostInfo)">
-      <div class="AddDiaryForm">
-        <h1>Edit Diary</h1>
-        <ul id="diarytitle">
-          <label for="diarytitle">Diary Title</label
-          ><br />
-          <input
-            v-model="PostInfo.title"
-            type="text"
-            id="diarytitle"
-            placeholder="제목"
-          />
-        </ul>
-        <ul id="diarydescription">
-          <label for="diarydescription">Description</label
-          ><br />
-          <input
-            v-model="PostInfo.description"
-            type="text"
-            id="diarydescription"
-            placeholder="다이어리 설명"
-          />
-        </ul>
-        <ul id="diaryimage">
-          <label for="diaryimage">Image</label
-          ><br />
-          <input type="file" id="diaryimage" @change="onFileChange" />
-        </ul>
-        <button class="submit">수정하기</button>
-      </div>
-    </form>
-  </div>
+  <v-container class="hnContainer">
+    <div class="hnDiary">
+      <v-card
+        dark
+        style="border-radius:17px"
+      >
+        <v-img 
+          :src="getCoverImage"
+          gradient="to bottom, rgba(0,0,0,.2), rgba(0,0,0,.1)"
+          alt="No Image"
+          class="hnImage"
+        >
+          <form @submit.prevent="editChannel(PostInfo)">
+            <div class="hnDiaryForm">
+              <div>
+                <v-text-field 
+                  label="diarytitle" 
+                  :rules="titleRules" 
+                  hide-details="auto" 
+                  counter="50"
+                  v-model="PostInfo.title"
+                  id="diarytitle"
+                  
+                ></v-text-field>
+              </div>
+              <div>
+                <v-textarea
+                  name="input-7-1"
+                  counter="200"
+                  :rules="desRules"
+                  label="diarydescription"
+                  v-model="PostInfo.description"
+                  id="diarydescription"
+                ></v-textarea>
+              </div>
+              <div>
+                <p class="hnCoverTxt">cover image</p>
+                <div class="hnCoverBox">
+                  <div class="hnCoverWrapper" style="width:10vw; height:10vw" v-for="img in defaultImages" :key="img.index">
+                    <img 
+                      class="hnCoverSample"
+                      @click="changeImage(img)"
+                      :src="baseImageAddr+img" alt="" >
+                  </div>
+                </div>
+              </div>
+              <div style="margin-top:1vh">
+                <v-file-input 
+                  small-chips accept="image/*" 
+                  label="diaryimage" 
+                  clearable prepend-icon="mdi-file-image"
+                  @change="onFileChange"
+                ></v-file-input>
+              </div>
+              <div>
+                <button class="submit">만들기</button>
+              </div>
+            </div>
+          </form>
+          <div class="hnBackBtn" @click="goHome">
+            <v-icon color="rgba(255, 255, 255, 0.9)">fas fa-arrow-left</v-icon>
+          </div>
+          <div class="ddBtn ddHamBtn" @click="goUserDetail">
+            <v-icon color="rgba(255, 255, 255, 0.9)" small>mdi-account</v-icon>
+          </div>
+        </v-img>
+      </v-card>
+    </div>
+  </v-container>
 </template>
 
 <script>
@@ -47,26 +82,70 @@ export default {
         title: null,
         description: null,
         file: null,
-        fileName: null,
-        channelId: null
-      }
+        fileName: this.getFileName ? this.getFileName : "default_channel1.jpg"
+      },
+      titleRules: [
+        value => !!value || "제목을 입력해 주세요 :)",
+        value => (value && value.length <= 50) || "제목은 최대 50자까지입니다."
+      ],
+      desRules: [
+        value => !!value || '설명을 입력해 주세요 :)',
+        value => (value && value.length <= 200) || '설명은 최대 200자까지입니다.'
+      ],
+      baseImageAddr: process.env.VUE_APP_STATIC_ADDR + "channel/default_channel",
+      defaultImages: [
+        '1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg'
+      ],
+      coverImage: process.env.VUE_APP_STATIC_ADDR + "channel/default_channel1.jpg",
+      file: null
     };
   },
   computed: {
-    ...mapGetters(["getSelectedChan"])
+    ...mapGetters(["getUserId", "getSelectedChan"]),
+    getCoverImage: function() {
+      if (this.file) {
+        const url = URL.createObjectURL(this.file)
+        return url
+      } else {
+        return this.coverImage
+      }
+    },
+    getFileName: function() {
+      if (this.file) {
+        return String(this.getUserId) + new Date().getTime() + ".jpg"
+      } else {
+        if (this.PostInfo.fileName) {
+          return this.PostInfo.fileName
+        } else {
+          return "default_channel1.jpg"
+        }
+      }
+    }
   },
   methods: {
-    ...mapActions(["editChannel", "bringChanDetail"]),
-    onFileChange(e) {
-      const files = e.target.files;
-      if (files) {
-        this.PostInfo.file = files[0];
-        this.PostInfo.fileName =
-          String(this.getUserId) + new Date().getTime() + ".jpg";
-      }
+    ...mapActions(["editChannel"]),
+    changeImage(img) {
+      this.file = null
+      this.coverImage = this.baseImageAddr+img
+      this.PostInfo.fileName = 'default_channel'+img
     },
     goHome() {
       router.push("/");
+    },
+    goUserDetail() {
+      router.push("userDetail");
+    },
+    onFileChange(file) {
+      if (file) {
+        this.PostInfo.file = file;
+        this.file = file
+        this.PostInfo.fileName =
+          String(this.getUserId) + new Date().getTime() + ".jpg";
+      } else {
+        this.PostInfo.file = null;
+        this.file = null;
+        this.PostInfo.fileName = "default_channel1.jpg";
+      }
     }
   },
   async created() {
--- front/src/components/editPost/editPost.vue
+++ front/src/components/editPost/editPost.vue
@@ -1,80 +1,69 @@
 <template>
-  <v-container v-if="getSelectedChan">
-    <v-row>
-      <v-col cols="2"
-        ><v-icon class="back" @click="goList">fas fa-arrow-left</v-icon></v-col
-      >
-      <v-col cols="10" class="diaryName"
-        ><h1>{{ getSelectedChan.title }}</h1></v-col
-      >
-    </v-row>
+  <div class="createPost">
+    <div class="createPostHeader">
+      <v-row>
+        <v-col cols="2"><v-icon class="back" @click="goList">
+          fas fa-arrow-left</v-icon></v-col>
+        <v-col cols="10" class="diaryName"></v-col>
+      </v-row>
+      <div>
+        <h2>{{ getChanName }}</h2>
+      </div>
+    </div>
     <form class="AddPostForm" @submit.prevent="editPost(postInfo)">
       <div class="AddPostForm">
-        <ul id="posttitle">
-          <label for="posttitle">Title</label
-          ><br />
-          <input
+          <v-text-field
             v-model="postInfo.title"
             type="text"
             id="posttitle"
-            placeholder="제목"
+            placeholder="title"
+          ></v-text-field>
+          <v-file-input prepend-icon="" append-outer-icon="mdi-camera" accept="video/*" id="postvideo" placeholder="upload your video" @change="onFileChange"></v-file-input>
+          <video
+            class="videoPreview"
+            :src="videoTempUrl"
+            controls="controls"
           />
-        </ul>
-        <ul id="postvideo">
-          <label for="postvideo">Video</label
-          ><br />
-          <input type="file" id="postvideo" @change="onFileChange" />
-          <div class="preview">
-            <video
-              class="videoPreview"
-              :src="videoTempUrl"
-              controls="controls"
-            />
-          </div>
-        </ul>
-        <ul id="posttag">
-          <label for="posttag">Tags</label
-          ><br />
-          <input
+          <v-text-field
             v-model="postInfo.tags"
             type="text"
             id="posttag"
-            placeholder="관련 태그"
-          />
-        </ul>
-        <ul id="postposs">
+            placeholder="tags"
+          ></v-text-field>
           <input type="checkbox" id="postposs" v-model="postInfo.possible" />
-          <label for="postposs">Comment 허용</label>
-        </ul>
+          <label class="optCheck" for="postposs">Accept comments</label>
         <ul id="saveVideo">
           <input type="checkbox" id="saveVideo" v-model="postInfo.saveVideo" />
-          <label for="saveVideo">영상 저장</label>
+          <label class="optCheck" for="saveVideo">Save video</label>
         </ul>
         <button class="submit">Upload</button>
       </div>
     </form>
-  </v-container>
+    <v-card class="loading" v-if="getPostLoading">
+      ...영상을 모델로 넘기고 있습니다...<br />
+      조금만 기다려 주세요.<br />
+      <v-progress-circular indeterminate color="green"></v-progress-circular>
+    </v-card>
+  </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import { mapGetters, mapMutations, mapActions } from "vuex";
 import router from "@/router";
 
 export default {
-  name: "editPost",
+  name: "createDiary",
   data() {
     return {
       postInfo: {
         title: null,
-        video: null,
         tags: null,
         context: "context",
         cover_image: "cover_image",
         possible: false,
         saveVideo: false,
         file: null,
-        fileName: null,
-        post_id: null
+        fileName: null
       },
       videoAddr: process.env.VUE_APP_STATIC_ADDR + "diary/",
       videoTempUrl: null
@@ -82,31 +71,45 @@ export default {
   },
   methods: {
     ...mapActions(["editPost"]),
+    ...mapMutations(["setChanName", "setChanId"]),
     goList() {
       router.push("/postList");
     },
     async onFileChange(e) {
-      const files = e.target.files;
-      if (files) {
-        const file = files[0];
+      const file = e;
+      if (file) {
+        // const file = files[0];
         this.postInfo.file = file;
         this.postInfo.fileName =
           String(this.getUserId) + new Date().getTime() + ".mp4";
         const blobFile = new Blob([file], { type: file.type });
+        // console.log(blobFile)
         this.videoTempUrl = URL.createObjectURL(blobFile);
+      } else {
+        this.postInfo.fileName = '';
+        this.videoTempUrl = '';
       }
     }
   },
   computed: {
-    ...mapGetters(["getSelectedChan", "getEditDiary"])
+    ...mapGetters(["getUserId", "getChanName", "getPostLoading", "getEditDiary"])
   },
   async created() {
+    const chanName = sessionStorage.getItem("chanName");
+    const chanId = sessionStorage.getItem("chan");
+    if (chanId) {
+      this.setChanName(chanName);
+      this.setChanId(chanId);
+    } else {
+      router.push("/");
+    }
     if (this.getEditDiary) {
       this.postInfo.title = this.getEditDiary.title;
       this.postInfo.tags = this.getEditDiary.tags;
       this.postInfo.possible = this.getEditDiary.is_use_comment;
       this.postInfo.saveVideo = this.getEditDiary.is_save_video;
       this.videoTempUrl = this.videoAddr + this.getEditDiary.video_file;
+      console.log(this.videoAddr + this.getEditDiary.video_file)
       this.postInfo.fileName = this.getEditDiary.video_file;
       this.postInfo.post_id = this.getEditDiary.pk;
     } else {
